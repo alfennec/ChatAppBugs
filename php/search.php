a9650deca668eed10615d702a0a62444016b08b9
= <dev.berrahal@gmail.com>
2024-05-26 18:28:16 +0100

    app final

--- php/search.php
+++ php/search.php
@@ -5,7 +5,12 @@
     $outgoing_id = $_SESSION['unique_id'];
     $searchTerm = mysqli_real_escape_string($conn, $_POST['searchTerm']);
 
-    $sql = "SELECT * FROM users WHERE NOT unique_id = {$outgoing_id} AND (fname LIKE '%{$searchTerm}%' OR lname LIKE '%{$searchTerm}%') ";
+    
+    ###### Ajouter du code ici : début
+    
+
+    ######  Fin code
+
     $output = "";
     $query = mysqli_query($conn, $sql);
     if(mysqli_num_rows($query) > 0){
